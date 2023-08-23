78c77115412eb967f1f28ab9d99810c19d70a5e6
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-08-23 10:29:42 -0700

    Update PS_AD_User_Importer.ps1

--- PS_AD_User_Importer.ps1
+++ PS_AD_User_Importer.ps1
@@ -1,8 +1,8 @@
 # Specify the path to the text file containing usernames (one username per line)
 $userListFilePath = "C:\Path\To\usernames.txt"
-$domain = "YourDomain"
+
 # Get the Active Directory "Users" group object
-$group = Get-ADGroup "Users" -Server $domain
+$group = Get-ADGroup "Users" -Server "Contoso"
 
 if ($group -eq $null) {
     Write-Host "Group 'Users' not found in Active Directory."
@@ -12,20 +12,36 @@ if ($group -eq $null) {
 # Read usernames from the text file
 $usernames = Get-Content $userListFilePath
 
-# Loop through each username and add them to the group
 foreach ($username in $usernames) {
-    # Check if the user exists
-    $user = Get-ADUser $username -ErrorAction SilentlyContinue -Server $domain
+    # Check if the user exists in Active Directory
+    $user = Get-ADUser $username -ErrorAction SilentlyContinue -Server "Contoso"
 
     if ($user -eq $null) {
-        Write-Host "User '$username' not found in Active Directory."
-    } else {
-        # Add the user to the group
-        Add-ADGroupMember -Identity $group -Members $user -ErrorAction SilentlyContinue
+        Write-Host "User '$username' not found in Active Directory. Adding user..."
+
+        # Add the user to Active Directory
+        $password = ConvertTo-SecureString "Password123!" -AsPlainText -Force
+        $userParams = @{
+            SamAccountName = $username
+            Name = $username
+            GivenName = $username
+            Surname = "User"
+            DisplayName = $username
+            UserPrincipalName = "$username@contoso.com"
+            Path = "OU=Users,DC=contoso,DC=com"
+            AccountPassword = $password
+            Enabled = $true
+        }
+        New-ADUser @userParams
+
+        # Add the user to the "Users" group
+        Add-ADGroupMember -Identity $group -Members $username
         if ($?) {
             Write-Host "User '$username' added to 'Users' group."
         } else {
             Write-Host "Failed to add user '$username' to 'Users' group."
         }
+    } else {
+        Write-Host "User '$username' already exists in Active Directory."
     }
 }
