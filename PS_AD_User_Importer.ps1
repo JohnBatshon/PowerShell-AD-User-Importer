6709f8e575bc1456dee692aa7d0e6fa1e0a3036f
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-08-23 10:09:06 -0700

    Update PS_AD_User_Importer.ps1

--- PS_AD_User_Importer.ps1
+++ PS_AD_User_Importer.ps1
@@ -7,21 +7,21 @@ $user_list = .\usernames.txt
 # --------------------------- #
 
 $password = ConvertTo-SecureString $user_password -AsPlainText -Force
-New-ADOrganizationalUnit -Name _Users_ -ProtectedFromAccidentalDeletion $false
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
 
-foreach ($name in $user_list) {
-    $first = $name.Split(" ")[0].ToLower()
-    $last = $name.Split(" ")[1].ToLower()
-    $username = "$(first.Substring(0,1))$last").ToLower()
-    Write-Host "Creating user: ($username)" -BackgroundColor Black -ForegroundColor Yellow
-
+foreach ($n in $user_list) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+    
     New-AdUser -AccountPassword $password `
-                -GivenName $first `
-                -Surname $last `
-                -DisplayName $username `
-                -Name $username `
-                -EmployeeID $username `
-                -PasswordNeverExpires $false `
-                -Path "ou=_Users_,$(([ADSI]`"").distinguisedName)" `
-                -Enabled $true
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
 }
