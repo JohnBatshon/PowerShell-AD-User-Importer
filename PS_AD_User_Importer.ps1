4ab2382222a96370738b4d4cdd0c612c0142757c
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-08-23 16:01:58 -0700

    Update PS_AD_User_Importer.ps1

--- PS_AD_User_Importer.ps1
+++ PS_AD_User_Importer.ps1
@@ -1,40 +1,37 @@
 # Path to CSV File
-$ADUsers = Import-csv C:\Temp\NewUsers.CSV
+$ADUsers = Import-csv C:\Temp\NewUsers.csv
 
-for each ($user in  $ADUsers)
-{
+foreach ($user in  $ADUsers) {
 
-        $Username = $User.username
-        $Password = $User.password
-        $Firstname = $User.firstname
-        $Lastname = $User.lastname
-        $Department = $User.department
-        $OU = $User.ou
+    $Username = $User.username
+    $Password = $User.password
+    $Firstname = $User.firstname
+    $Lastname = $User.lastname
+    $Department = $User.department
+    $OU = $User.ou
 
-        # Check to see if user account exists in Active Directory
-        if (Get-ADUser -F {SamAccountName -eq $Username})
-        {
-                # If user does exist then output a warning message
-                Write-Warning "A user account named $Username already exists within Active Directory"
-        }
-        else 
-        {
-                # If the user does not exist then create a new user account
-                
+    # Check to see if user account exists in Active Directory
+    if (Get-ADUser -Filter {SamAccountName -eq $Username}) {
+        # If user does exist then output a warning message
+        Write-Warning "A user account named $Username already exists within Active Directory"
+    }
+    else {
+        # If the user does not exist then create a new user account
+        
         # Account will be created in the OU listed in the $OU variable in the CSV file.
         # Ensure you change the domain name in the "-UserPrincipalName" variable
-                New-ADUser `
-                -SamAccountName $Username `
-                -UserPrincipalName "$username@SecureCo.com" `
-                -Name "$Firstname $Lastname" `
-                -GivenName $Firstname `
-                -Surname $Lastname `
-                -Enabled $true `
-                -ChangePasswordAtLogin $true `
-                -DisplayName "$Lastname, $Firstname" `
-                -Department $Department `
-                -Path $OU `
-                -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+        New-ADUser `
+        -SamAccountName $Username `
+        -UserPrincipalName "$username@SecureCo.com" `
+        -Name "$Firstname $Lastname" `
+        -GivenName $Firstname `
+        -Surname $Lastname `
+        -Enabled $true `
+        -ChangePasswordAtLogin $true `
+        -DisplayName "$Lastname, $Firstname" `
+        -Department $Department `
+        -Path $OU `
+        -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force)
                 
-        }
+    }
 }
