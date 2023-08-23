19d5fc463e2f1cd0d94a87e8f115599eac2b6839
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-08-23 10:16:34 -0700

    Update PS_AD_User_Importer.ps1

--- PS_AD_User_Importer.ps1
+++ PS_AD_User_Importer.ps1
@@ -1,27 +1,31 @@
-# ----- Variables Used ----- #
-# Adjust the password as needed. 
-$user_password = "Password1?"
-# Text file with First & Last names.
-# Format: First Last
-$user_list = .\usernames.txt
-# --------------------------- #
+# Specify the path to the text file containing usernames (one username per line)
+$userListFilePath = "C:\Path\To\usernames.txt"
 
-$password = ConvertTo-SecureString $user_password -AsPlainText -Force
-New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
+# Get the Active Directory "Users" group object
+$group = Get-ADGroup "Users" -Server "YourDomain"
 
-foreach ($n in $user_list) {
-    $first = $n.Split(" ")[0].ToLower()
-    $last = $n.Split(" ")[1].ToLower()
-    $username = "$($first.Substring(0,1))$($last)".ToLower()
-    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
-    
-    New-AdUser -AccountPassword $password `
-               -GivenName $first `
-               -Surname $last `
-               -DisplayName $username `
-               -Name $username `
-               -EmployeeID $username `
-               -PasswordNeverExpires $true `
-               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
-               -Enabled $true
+if ($group -eq $null) {
+    Write-Host "Group 'Users' not found in Active Directory."
+    exit
+}
+
+# Read usernames from the text file
+$usernames = Get-Content $userListFilePath
+
+# Loop through each username and add them to the group
+foreach ($username in $usernames) {
+    # Check if the user exists
+    $user = Get-ADUser $username -ErrorAction SilentlyContinue -Server "YourDomain"
+
+    if ($user -eq $null) {
+        Write-Host "User '$username' not found in Active Directory."
+    } else {
+        # Add the user to the group
+        Add-ADGroupMember -Identity $group -Members $user -ErrorAction SilentlyContinue
+        if ($?) {
+            Write-Host "User '$username' added to 'Users' group."
+        } else {
+            Write-Host "Failed to add user '$username' to 'Users' group."
+        }
+    }
 }
